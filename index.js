95fb94d734cb86999fbe45a6c2c9e348106d8dd6
ban hussein <banhussien97@gmail.com>
2019-10-19 20:02:32 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,3 +3,7 @@ function BoardMember(name,homeState,training){
   this.homeState=homeState;
   this.training=training;
 }
+//User.prototype.sayHello = function() {
+BoardMember.prototype.veto=function(){
+  return 
+}
