7fc7c5e37c4c4315f3898a13bde1584157cb3bff
ban hussein <banhussien97@gmail.com>
2019-10-19 20:09:29 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ BoardMember.prototype.approve=function(){
   return "You can do that!"
 }
 BoardMember.prototype.doCharity=function(){
-  return " I like to help people."
+  return "I like to help people."
 }
 BoardMember.prototype.releasePressStatement=function(){
   return "You will see great things from Scuber."
